e0e92260957a8554d429090f8a56b340902e99c6
Spikree <avimahalingpure10@gmail.com>
2024-02-29 20:58:39 +0530

    removed useless code

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,17 +13,13 @@ function Cart() {
         });
     }
 
-    function getCarId(id) {
-        console.log(id)
-    }
-
     if (cart.length === 0) {
         return <>
         <EmptyCart />
     </>
     } else if (cart.length != 0) {
         return <>
-            <CartWithCars insertCar={getCarId}/>
+            <CartWithCars/>
         </>
     }
 }
--- src/components/RentalCars.jsx
+++ src/components/RentalCars.jsx
@@ -3,17 +3,13 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 function RentalCars(props) {
 
-    function returnIdForCart() {
-        props.insertCar(props.id);
-    }
-
     return <>
     <div className="card">
     <img src={props.imageLink} alt="Car Image" />
     <div className="card-content">
     <div className="card-title">{props.carName}</div>
     <div className="card-text">{props.carPrice}</div>
-    <div className="icon-cart" onClick={returnIdForCart}><FontAwesomeIcon className="icon-cart" icon={props.icon} /></div>
+    <div className="icon-cart"><FontAwesomeIcon className="icon-cart" icon={props.icon} /></div>
     </div>
     </div>
     </>
