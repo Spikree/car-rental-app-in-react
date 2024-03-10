3df47ca393aa0eff62e482152cf5f3533393e39c
Spikree <avimahalingpure10@gmail.com>
2024-03-10 21:53:10 +0530

    trying to add cart functionality

--- src/components/RentalCars.jsx
+++ src/components/RentalCars.jsx
@@ -1,18 +1,34 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 function RentalCars(props) {
+  const [cars, addCars] = useState([]);
 
-    return <>
-    <div className="card">
-    <img src={props.imageLink} alt="Car Image" />
-    <div className="card-content">
-    <div className="card-title">{props.carName}</div>
-    <div className="card-text">{props.carPrice}</div>
-    <div className="icon-cart"><FontAwesomeIcon className="icon-cart" icon={props.icon} /></div>
-    </div>
-    </div>
+  function addToCart(car) {
+    addCars((prev) => [...prev, car]);
+  }
+
+  console.log(cars);
+
+  return (
+    <>
+      <div className="card">
+        <img src={props.imageLink} alt="Car Image" />
+        <div className="card-content">
+          <div className="card-title">{props.carName}</div>
+          <div className="card-text">{props.carPrice}</div>
+          <div className="icon-cart">
+            <FontAwesomeIcon
+              onClick={() => addToCart(props.id)}
+              className="icon-cart"
+              icon={props.icon}
+            />
+          </div>
+        </div>
+      </div>
     </>
+  );
 }
 
 export default RentalCars;
